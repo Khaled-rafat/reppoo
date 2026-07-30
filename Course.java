134cf70519cda4b4ccd87c2fb39349e70b6c1f8a
Khaled <rs424239366@gmail.com>
2026-07-30 23:03:49 +0300

    complete

---
+++ Course.java
@@ -0,0 +1,61 @@
+public class Course {
+
+    private String courseId;
+    private String courseName;
+    private int duration;
+    private Trainer trainer;
+
+    // Default Constructor
+    public Course() {
+
+    }
+
+    // Parameterized Constructor
+    public Course(String courseId, String courseName, int duration, Trainer trainer) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.duration = duration;
+        this.trainer = trainer;
+    }
+
+    // Getters
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
+
+    // Setters
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setTrainer(Trainer trainer) {
+        this.trainer = trainer;
+    }
+
+    // toString
+    @Override
+public String toString() {
+
+    return "Course{" +"courseId = " + courseId +", courseName = " + courseName +", duration = " + duration +", trainer = " + (trainer != null ? trainer.getName() : "No Trainer") +'}';
+}
+}
